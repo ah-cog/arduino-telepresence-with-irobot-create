6c64d8d20a813a6184f071d7a240f72f61a1c739
Michael Gubbels <mrgubbels@gmail.com>
2014-04-20 13:19:16 -0400

    Built basic iRobot Create Web Server.

--- Examples/iRobotCreate_WiFiWebServer/iRobotCreate_WiFiWebServer.ino
+++ Examples/iRobotCreate_WiFiWebServer/iRobotCreate_WiFiWebServer.ino
@@ -85,8 +85,16 @@ boolean setupRoomba() {
   roomba.fullMode();
 }
 
+uint8_t buf[52];                                        // Packet 6 returns 52 bytes
 
 void loop() {
+  
+  // Get sensor data (from iRobot Create)
+  boolean hasRoombaSensorData = roomba.getSensors(6 , buf , 52);             // packetID, destination, length (bytes) 
+                                                          // Note that getSensors() -returns true when [length] bytes have been read 
+                                                          //                        -is blocking until [length] bytes have been read or timeout occurs
+                                                          // Consult Open Interface Manual pg 17 for packet lengths
+  
   // listen for incoming clients
   WiFiClient client = server.available();
   if (client) {
@@ -97,6 +105,7 @@ void loop() {
       if (client.available()) {
         char c = client.read();
         Serial.write(c);
+        
         // if you've gotten to the end of the line (received a newline
         // character) and the line is blank, the http request has ended,
         // so you can send a reply
@@ -105,23 +114,239 @@ void loop() {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
           client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          // client.println("Refresh: 5");  // refresh the page automatically every 5 sec
           client.println();
           client.println("<!DOCTYPE HTML>");
           client.println("<html>");
           // output the value of each analog input pin
-          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
-            int sensorReading = analogRead(analogChannel);
-            client.print("analog input ");
-            client.print(analogChannel);
-            client.print(" is ");
-            client.print(sensorReading);
-            client.println("<br />");       
+//          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+//            int sensorReading = analogRead(analogChannel);
+//            client.print("analog input ");
+//            client.print(analogChannel);
+//            client.print(" is ");
+//            client.print(sensorReading);
+//            client.println("<br />");       
+//          }
+
+          // Print the state of the iRobot Create sensors
+          if (hasRoombaSensorData) {
+            client.println("<h1>iRobot Create Web Server</h1>");
+            client.println("<h2>Packet 6 (Full) Sensor Data</h2>");
+              
+            client.print("<h3>Bumps and Wheel Drops: ");
+            client.println(buf[0], BIN);
+            client.print("</h3>");
+            client.print("Wheeldrop Caster: ");
+            client.println(bitRead(buf[0], 4));
+            client.print("<br />");
+            client.print("Wheeldrop Left: ");
+            client.println(bitRead(buf[0], 3));   
+            client.print("<br />");
+            client.print("Wheeldrop Right: ");
+            client.println(bitRead(buf[0], 2));  
+            client.print("<br />");   
+            client.print("Bump Left: ");
+            client.println(bitRead(buf[0], 1));
+            client.print("<br />");
+            client.print("Bump Right: ");
+            client.println(bitRead(buf[0], 0));
+            client.print("<br />");
+             
+            client.print("<h3>Wall Sensor:");
+            client.println(buf[1], BIN);
+            client.print("</h3>");
+        
+            client.print("Cliff Left: ");
+            client.println(buf[2], BIN);  
+            client.print("<br />");
+            client.print("Cliff Front Left: ");  
+            client.println(buf[3], BIN);   
+            client.print("<br />");
+            client.print("Cliff Front Right: ");  
+            client.println(buf[4], BIN);  
+            client.print("<br />");
+            client.print("Cliff Right: ");    
+            client.println(buf[5], BIN);
+            client.print("<br />");
+        
+            client.print("<h3>Virtual Wall: ");
+            client.println(buf[6], BIN);
+            client.print("</h3>");
+        
+            client.print("<h3>Low Side Drivers and Wheel Overcurrents: ");
+            client.println(buf[7], BIN);
+            client.print("</h3>");
+        
+            // Note the 2 unused bytes pg 18 of Open Interface Manual
+            client.print("<h3>Infrared Byte: ");
+            client.println(buf[10], BIN);
+            client.print("</h3>");
+        
+            client.print("<h3>Buttons: ");
+            client.println(buf[11], BIN);
+            client.print("</h3>");
+            client.print("Play Button: ");
+            client.println(bitRead(buf[11], 0));
+            client.print("<br />");
+            client.print("Advance Button: ");
+            client.println(bitRead(buf[11], 2));
+            client.print("<br />");
+            client.print("<br />");
+             
+            client.print("Distance (mm): ");              // Sum of distance traveled by both wheels divided by two
+            x = BitShiftCombine(buf[12], buf[13]);        // Value sent as 16 bit signed value high byte first.
+            client.println(x);                            // Note that if note called frequently, value is capped at min or max
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Angle (Degrees- CCW+): ");          
+            x = BitShiftCombine(buf[14], buf[15]);        // Value sent as 16 bit signed value high byte first.
+            client.println(x);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Charging State: ");
+            u = buf[16];
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Battery Voltage (mV): ");
+            u = BitShiftCombine(buf[17], buf[18]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Battery Current (mA): ");
+            x = BitShiftCombine(buf[19], buf[20]);      // Value sent as 16 bit signed value high byte first.
+            client.println(x);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Battery Temperature (C): ");
+            x = buf[21];
+            client.println(x);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Current Battery Charge (mAh): ");
+            u = BitShiftCombine(buf[22], buf[23]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Battery Capacity (mAh): ");
+            u = BitShiftCombine(buf[24], buf[25]);      // Value sent as 16 bit signed value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Wall Signal Strength (0-4095): ");
+            u = BitShiftCombine(buf[26], buf[27]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Cliff Left Signal Strength (0-4095): ");
+            u = BitShiftCombine(buf[28], buf[29]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u); 
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Cliff Front Left Signal Strength (0-4095): ");
+            u = BitShiftCombine(buf[30], buf[31]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Cliff Front Right Signal Strength (0-4095): ");
+            u = BitShiftCombine(buf[32], buf[33]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Cliff Right Signal Strength (0-4095): ");
+            u = BitShiftCombine(buf[34], buf[35]);      // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Cargo Bay Digital Inputs: ");
+            client.println(buf[36], BIN);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Cargo Bay Analog Signal (0-1023):");
+            u = BitShiftCombine(buf[37], buf[38]);       // Value sent as 16 bit unsigned value high byte first.
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+  
+            client.print("Charging Sources Available:");
+            client.println(buf[39], BIN);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("OI Mode: ");
+            u = buf[40];
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Song Number: ");
+            x = buf[41];
+            client.println(x);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Song Playing: ");
+            client.println(buf[42], BIN);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Number of Stream Packets: ");
+            u = buf[43];
+            client.println(u);
+            client.print("<br />");
+            client.print("<br />");
+        
+            client.print("Requested Velocity (mm/s): ");
+            x = BitShiftCombine(buf[44], buf[45]);      // Value sent as 16 bit signed value high byte first.
+            client.println(x);  
+            client.print("<br />");   
+            client.print("<br />");     
+        
+        
+            client.print("Requested Radius (mm):");
+            x = BitShiftCombine(buf[46], buf[47]);      // Value sent as 16 bit signed value high byte first.
+            client.println(x);     
+            client.print("<br />");  
+            client.print("<br />");   
+        
+        
+            client.print("Requested Right Velocity (mm/s): ");
+            x = BitShiftCombine(buf[48], buf[49]);      // Value sent as 16 bit signed value high byte first.
+            client.println(x);       
+            client.print("<br />");  
+            client.print("<br />"); 
+        
+        
+            client.print("Requested Left Velocity (mm/s): ");
+            x = BitShiftCombine(buf[50], buf[51]);      // Value sent as 16 bit signed value high byte first.
+            client.println(x);       
+            client.print("<br />");  
+            client.print("<br />"); 
           }
+        
           client.println("</html>");
-           break;
+          break;
         }
         if (c == '\n') {
+          
+          // Parse HTTP request
+          
+          
+          
           // you're starting a new line
           currentLineIsBlank = true;
         } 
@@ -140,12 +365,8 @@ void loop() {
   }
   
   
-  uint8_t buf[52];                                        // Packet 6 returns 52 bytes
-  bool ret = roomba.getSensors(6 , buf , 52);             // packetID, destination, length (bytes) 
-                                                          // Note that getSensors() -returns true when [length] bytes have been read 
-                                                          //                        -is blocking until [length] bytes have been read or timeout occurs
-                                                          // Consult Open Interface Manual pg 17 for packet lengths
-  if (ret)
+  
+  if (hasRoombaSensorData)
   {
     Serial.println("\n\n\n\n\n\n");
     Serial.println("iRobot Create Packet 6 (Full) Sensor Data \n");
